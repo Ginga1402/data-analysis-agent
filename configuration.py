5c81d930cceda9d188f647a74b7f3d8174494d2d
Ishaan Kohli <130181481+Ginga1402@users.noreply.github.com>
2025-10-22 15:31:53 +0530

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,1 +1,16 @@
+DEFAULT_FIGURE_SIZE = (6,4)
+DEFAULT_DPI = 100
+MAX_RESULT_DISPLAY_LENGTH = 300
+
+
+
+
+
+
+
+BASEURL = "http://localhost:11434"
+query_classification_model_name = "mistral:7b-instruct-q4_K_M"
+pandas_writing_model = "qwen2.5-coder:7b-instruct-q4_K_M"
+reasoning_agent_model = "qwen2.5:7b-instruct-q4_K_M"
+
 
